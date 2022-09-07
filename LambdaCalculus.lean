4d8fd9ab740407baeff288fdbf6b6d8d36e2a1ad
Siddharth Bhat <siddu.druid@gmail.com>
2022-09-07 02:13:37 +0100

    add stubs

---
+++ LambdaCalculus.lean
@@ -0,0 +1,10 @@
+/-
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Siddharth Bhat
+-/
+
+/-
+Implementation of a partial evaluator for a simple
+lambda calculus with fixpoints.
+-/
+
